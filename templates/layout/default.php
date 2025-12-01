0ab279dd1cc1df9578c4420de4413de62dfdf75d
Roman Sidorkin <roman.sidorkin@gmail.com>
2025-12-01 17:28:31 +0200

    Add missing title tag in default layout

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -15,6 +15,7 @@ $lang = I18n::getLocale();
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
+    <title><?= $this->fetch('title') ?></title>
     <?= $this->fetch('meta') ?>
     <?= $this->element('Brammo/Admin.Layout/css') ?>
     <?= $this->Html->css('Brammo/Admin.styles') ?>
