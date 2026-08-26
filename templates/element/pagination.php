23395ca056012f8c66b3825f71ff4b5ba795f487
Roman Sidorkin <roman.sidorkin@gmail.com>
2026-08-26 18:44:57 +0300

    Update pagination template to conditionally display pagination links only when total pages exceed one, improving user experience.

--- templates/element/pagination.php
+++ templates/element/pagination.php
@@ -5,20 +5,18 @@
  * 
  * @var \Brammo\Admin\View\AppView $this
  */
-
-// If total count of pages <= 1, don't show pagination
-if ($this->Paginator->total() <= 1) {
-    return;
-}
 ?>
 <div class="d-lg-flex justify-content-between">
-    <?= $this->Paginator->links([
+    <?php 
+    if ($this->Paginator->total() > 1) {
+        echo $this->Paginator->links([
             'first' => $this->Html->icon('chevron-double-left'),
             'prev' => $this->Html->icon('chevron-left'),
             'next' => $this->Html->icon('chevron-right'),
             'last' => $this->Html->icon('chevron-double-right'),
             'escape' => false
-        ]) 
+        ]);
+    }
     ?>
     <div class="mt-3 mt-lg-0">
         <?= $this->Paginator->counter(__d('brammo/admin', 'Page {{page}} of {{pages}}, showing {{current}} record(s) out of {{count}} total')) ?>
